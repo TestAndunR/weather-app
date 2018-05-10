514d3f9a647bbe36f03836edc92e6d508cc3fb5a
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-10 12:48:08 +0530

    enable lambda proxy integrtion

--- weather-app/lambda.js
+++ weather-app/lambda.js
@@ -2,16 +2,25 @@ let AWS = require('aws-sdk');
 let request = require('request');
 exports.handler = function(event, context, callback) {
 
-	let city = event.queryStringParameters.city;
+	let city = event.queryStringParameters.City;
 
 	let apiKey = "e56ee13c7129aba6b847905b40afec56";
 	let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+	let res = {
+		body: "",
+		statusCode: 200,
+		isBase64Encoded: false
+	}
 
 	request(apiUrl,function(err,response,body){
 		if (err){
 			console.log("error",err);
+			res.body = JSON.stringify(body);
+			callback(res,null);
 		}else{
 			console.log(body);
+			res.body = JSON.stringify(body);
+			callback(null,res);
 		}
 	})
 
